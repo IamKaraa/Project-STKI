2167da596b0f2b9d85dd6296b58b051245690bbf
Karaa <iamkara2813@gmail.com>
2026-06-16 19:39:14 +0700

    rapihin struktur

--- stki-frontend/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SearchController;
-
-Route::get('/', [SearchController::class, 'index'])->name('home');
-Route::get('/all-documents', [SearchController::class, 'showAll'])->name('all.documents');
